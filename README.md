9cb0bd28d2024060779fac00325a622988bb1e01
Dan Croak <dan@statusok.com>
2020-03-25 18:41:07 -0700

    main.go: move static blog generator into repo
    An experiment to see if this aids in rapid development.
    Previously at https://github.com/statusok/genblog
    
    #8

--- README.md
+++ README.md
@@ -1,3 +1,246 @@
-# dancroak.com blog
+# blog
 
-Articles about making software.
+Articles about making software at <https://dancroak.com>.
+
+# Setup
+
+[Install Go](https://dancroak.com/install-or-upgrade-go-on-macos).
+Then, run:
+
+```
+go install ./...
+```
+
+This installs a `blog` command-line program:
+
+```
+usage:
+  blog add <article-url-slug>
+  blog serve
+  blog build
+```
+
+`blog` is a static personal blog generator featuring:
+
+* Markdown files with no front matter
+* Local preview server
+* Images
+* Embedded code blocks
+* Drafts
+* Tags
+* "Last updated" timestamp
+* Redirects
+* `rel=canonical` tags
+* Responsive design
+* PageSpeed Insights performance score of 100
+* Mozilla Observatory security grade of A+
+
+## Write
+
+Add an article:
+
+```
+blog add example-article
+```
+
+Edit `articles/example-article.md` in a text editor.
+It is a [GitHub-Flavored Markdown](https://github.github.com/gfm/) file
+with no front matter.
+
+The first line of the file is the article title.
+It must be an `<h1>` tag:
+
+```md
+# Example Article
+```
+
+Preview at <http://localhost:2000> with:
+
+```
+./blog serve
+```
+
+See the [JSON feed](https://jsonfeed.org/) at <http://localhost:2000/feed.json>.
+
+Add images to the `articles/images` directory.
+Refer to them in articles via relative path:
+
+```md
+![alt text](images/example.png)
+```
+
+In addition to
+[fenced code blocks](https://github.github.com/gfm/#fenced-code-blocks),
+`./blog` recognizes a special `embed`
+[info string](https://github.github.com/gfm/#info-string).
+This Markdown...
+
+    Instantiate a client:
+
+    ```embed
+    example.rb instantiate
+    ```
+
+...embeds code from `articles/code/example.rb`
+between `begindoc` and `enddoc` magic comments:
+
+```ruby
+# begindoc: instantiate
+require 'example-sdk'
+
+client = Example::Client.new(
+  credential: '...',
+  name: 'example',
+)
+# enddoc: instantiate
+```
+
+The magic comments demarcate code blocks by id.
+In this example, the id is `instantiate`.
+
+This allows you to run, lint, and test embedded code
+separate from Markdown prose.
+
+## Configure
+
+Configure blog in `config.json`:
+
+```json
+{
+  "blog": {
+    "name": "Dan Croak",
+    "url": "https://dancroak.com"
+  },
+  "articles": [
+    {
+      "id": "article-is-draft-if-published-is-future-date",
+      "published": "2050-01-01"
+    },
+    {
+      "id": "article-with-anonymous-author",
+      "published": "2018-04-15"
+    },
+    {
+      "author": "Alice",
+      "id": "article-with-single-author",
+      "published": "2018-04-01"
+    },
+    {
+      "author": "Alice and Bob",
+      "id": "article-with-multiple-authors",
+      "published": "2018-03-15"
+    },
+    {
+      "id": "article-with-tags",
+      "published": "2018-03-01",
+      "tags": [
+        "go",
+        "unix"
+      ]
+    },
+    {
+      "id": "article-with-updated-date",
+      "published": "2018-02-15",
+      "updated": "2018-02-20"
+    },
+    {
+      "id": "article-with-redirects",
+      "published": "2018-02-01",
+      "redirects": [
+        "/article-original-name",
+        "/article-renamed-again",
+        "/this-feature-works-only-on-netlify",
+      ]
+    },
+    {
+      "canonical": "https://seo.example.com/avoid-duplicate-content-penalty",
+      "id": "article-with-rel-canonical",
+      "published": "2018-01-15"
+    }
+  ]
+}
+```
+
+## Modify theme
+
+The `theme` directory's files can be modified
+to customize the blog's HTTP headers, HTML, CSS, and JavaScript.
+
+```
+.
+├── index.html
+└── public
+    ├── _headers
+    └── favicon.ico
+```
+
+The `_headers` file is copied to `public/_headers` to be used as
+[Netlify Headers](https://www.netlify.com/docs/headers-and-basic-auth/).
+
+The `.html` files
+are parsed as [Go templates](https://gowebexamples.com/templates/)
+by `blog`.
+
+The `article.html` file accepts a data structure like this:
+
+```
+{
+  Blog: {
+    "name": "Dan Croak",
+    "url": "https://dancroak.com"
+  }
+  Article: {
+    Author:        "Alice",
+    Body:          "<p>Hello, world.</p>",
+    Canonical:     "https://seo.example.com/avoid-duplicate-content-penalty"
+    ID:            "example-article",
+    LastUpdated:   "2018-04-15",
+    LastUpdatedIn: "2018 April",
+    LastUpdatedOn: "April 15, 2018",
+    Published:     "2018-04-10",
+    Tags:          ["go", "unix"],
+    Title:         "Example Article",
+    Updated:       "2018-04-15",
+  }
+}
+```
+
+The `index.html` file accepts a data structure like this:
+
+```
+{
+  Blog: {
+    "name": "Dan Croak",
+    "url": "https://dancroak.com"
+  },
+  Articles: [
+    {
+      Author:        "Alice",
+      Body:          "<p>Hello, world.</p>",
+      Canonical:     "https://seo.example.com/avoid-duplicate-content-penalty"
+      ID:            "example-article",
+      LastUpdated:   "2018-04-15",
+      LastUpdatedIn: "2018 April",
+      LastUpdatedOn: "April 15, 2018",
+      Published:     "2018-04-10",
+      Tags:          ["go", "unix"],
+      Title:         "Example Article",
+      Updated:       "2018-04-15",
+    }
+  ],
+  Tags: ["go", "unix"],
+}
+```
+
+## Publish
+
+Configure [Netlify](https://www.netlify.com):
+
+* Repository: `https://github.com/croaky/blog`
+* Branch: `master`
+* Build Cmd: `go run main.go build`
+* Public folder: `public`
+
+To publish articles, commit and push to the GitHub repo.
+
+View deploy logs in the Netlify web interface.
---
+++ main.go
@@ -0,0 +1,446 @@
+/*
+
+Add an article:
+
+  blog add <article-url-slug>
+
+Serve site on localhost:
+
+  blog serve
+
+Build site (HTML, images, code, Netlify files) to `public/`:
+
+  blog build
+
+*/
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/user"
+	"path"
+	"regexp"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/kr/jsonfeed"
+	"github.com/russross/blackfriday"
+)
+
+var wd string
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+	}
+	var err error
+	wd, err = os.Getwd()
+	check(err)
+
+	switch os.Args[1] {
+	case "add":
+		if len(os.Args) != 3 {
+			usage()
+		}
+		id := os.Args[2]
+		add(id)
+		fmt.Println("blog: Added article at ./articles/" + id + ".md")
+	case "serve":
+		fmt.Println("blog: Serving blog at http://localhost:2000")
+		serve(":2000")
+	case "build":
+		build()
+		fmt.Println("blog: Built blog at ./public")
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	const s = `usage:
+  blog add <article-url-slug>
+  blog serve
+  blog build
+`
+	fmt.Fprint(os.Stderr, s)
+	os.Exit(2)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func exitWith(s string) {
+	fmt.Println(s)
+	os.Exit(1)
+}
+
+// Blog contains data loaded from config.json
+type Blog struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+// Article contains data loaded from config.json and parsed Markdown
+type Article struct {
+	Author        string        `json:"author"`
+	Body          template.HTML `json:"-"`
+	Canonical     string        `json:"canonical,omitempty"`
+	ID            string        `json:"id"`
+	LastUpdated   string        `json:"-"`
+	LastUpdatedIn string        `json:"-"`
+	LastUpdatedOn string        `json:"-"`
+	Published     string        `json:"published"`
+	Redirects     []string      `json:"redirects,omitempty"`
+	Tags          []string      `json:"tags,omitempty"`
+	Title         string        `json:"-"`
+	Updated       string        `json:"updated,omitempty"`
+}
+
+func add(id string) {
+	blog, articles, _, _ := load()
+
+	noDashes := strings.Replace(id, "-", " ", -1)
+	noUnderscores := strings.Replace(noDashes, "_", " ", -1)
+	title := strings.Title(noUnderscores)
+	content := []byte("# " + title + "\n\n\n")
+	check(ioutil.WriteFile(wd+"/articles/"+id+".md", content, 0644))
+
+	u, err := user.Current()
+	check(err)
+
+	a := Article{
+		Author:    u.Name,
+		ID:        id,
+		Published: time.Now().Format("2006-01-02"),
+	}
+
+	data := struct {
+		Blog     Blog      `json:"blog"`
+		Articles []Article `json:"articles"`
+	}{
+		Blog:     blog,
+		Articles: append([]Article{a}, articles...),
+	}
+	config, err := json.MarshalIndent(data, "", "  ")
+	check(err)
+	check(ioutil.WriteFile(wd+"/config.json", config, 0644))
+}
+
+func serve(addr string) {
+	http.HandleFunc("/", handler)
+	check(http.ListenAndServe(addr, nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	redirectMap := build()
+
+	fmt.Println("blog: " + r.Method + " " + r.URL.Path)
+
+	for k, v := range redirectMap {
+		if r.URL.Path == k {
+			http.Redirect(w, r, v, 302)
+		}
+	}
+
+	// convert URLs like /intro to /intro.html for http.FileServer
+	if r.URL.Path != "/" && path.Ext(r.URL.Path) == "" {
+		r.URL.Path = r.URL.Path + ".html"
+	}
+
+	// use same headers as production, if present
+	for k, v := range headers() {
+		w.Header().Set(k, v)
+	}
+
+	fs := http.FileServer(http.Dir(wd + "/public"))
+	fs.ServeHTTP(w, r)
+}
+
+func headers() map[string]string {
+	result := make(map[string]string)
+
+	dat, err := ioutil.ReadFile(wd + "/theme/public/_headers")
+	if err != nil {
+		return result
+	}
+
+	for i, line := range strings.Split(string(dat), "\n") {
+		if i == 0 { // ignore first line ("/*\n")
+			continue
+		}
+		parts := strings.SplitN(line, ": ", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		k := strings.TrimSpace(parts[0])
+		v := strings.TrimSpace(parts[1])
+		result[k] = v
+	}
+
+	return result
+}
+
+func build() map[string]string {
+	blog, articles, tags, redirectMap := load()
+
+	// public directories
+	check(os.Mkdir(wd+"/public", os.ModePerm))
+	dir, err := ioutil.ReadDir(wd + "/public")
+	check(err)
+	for _, d := range dir {
+		os.RemoveAll(path.Join([]string{"public", d.Name()}...))
+	}
+	check(os.Mkdir(wd+"/public/images", os.ModePerm))
+
+	// index page
+	indexPage := template.Must(template.ParseFiles(wd + "/theme/index.html"))
+	f, err := os.Create("public/index.html")
+	check(err)
+	indexData := struct {
+		Blog     Blog
+		Articles []Article
+		Tags     []string
+	}{
+		Blog:     blog,
+		Articles: articles,
+		Tags:     tags,
+	}
+	check(indexPage.Execute(f, indexData))
+
+	// feed and article pages
+	feed := jsonfeed.Feed{
+		Title:       blog.Name,
+		HomePageURL: blog.URL,
+		FeedURL:     blog.URL + "/feed.json",
+	}
+	feed.Items = make([]jsonfeed.Item, len(articles))
+	articlePage := template.Must(template.ParseFiles(wd + "/theme/article.html"))
+	for i, a := range articles {
+		f, err := os.Create("public/" + a.ID + ".html")
+		check(err)
+		articleData := struct {
+			Blog    Blog
+			Article Article
+		}{
+			Blog:    blog,
+			Article: a,
+		}
+		check(articlePage.Execute(f, articleData))
+		item := jsonfeed.Item{
+			ID:          blog.URL + "/" + a.ID,
+			URL:         blog.URL + "/" + a.ID,
+			Title:       a.Title,
+			ContentHTML: string(a.Body),
+			Tags:        a.Tags,
+		}
+		published, err := time.Parse("2006-01-02", a.Published)
+		if err == nil {
+			item.DatePublished = published
+		}
+		updated, err := time.Parse("2006-01-02", a.LastUpdated)
+		if err == nil {
+			item.DateModified = updated
+		}
+		item.Author = &jsonfeed.Author{Name: a.Author}
+		feed.Items[i] = item
+	}
+	f, err = os.Create("public/feed.json")
+	check(err)
+	check(json.NewEncoder(f).Encode(&feed))
+
+	// images
+	cmd := exec.Command("cp", "-a", wd+"/articles/images/.", wd+"/public/images")
+	cmd.Run()
+
+	// headers, favicon.ico, and additional files from theme authors
+	cmd = exec.Command("cp", "-a", wd+"/theme/public/.", wd+"/public")
+	cmd.Run()
+
+	// redirects
+	redirects := ""
+	for k, v := range redirectMap {
+		redirects = redirects + k + " " + v + "\n"
+	}
+	check(ioutil.WriteFile(wd+"/public/_redirects", []byte(redirects), 0644))
+
+	return redirectMap
+}
+
+func load() (Blog, []Article, []string, map[string]string) {
+	config, err := ioutil.ReadFile(wd + "/config.json")
+	check(err)
+	var data struct {
+		Blog     Blog      `json:"blog"`
+		Articles []Article `json:"articles"`
+	}
+	check(json.Unmarshal(config, &data))
+
+	articles := make([]Article, len(data.Articles))
+	tags := make([]string, 0)
+	redirectMap := make(map[string]string)
+
+	for i, a := range data.Articles {
+		title, body := preProcess("articles/" + a.ID + ".md")
+		markdown := blackfriday.Run([]byte(body))
+
+		lastUpdated := a.Published
+		if a.Updated != "" {
+			lastUpdated = a.Updated
+		}
+		t, err := time.Parse("2006-01-02", lastUpdated)
+		check(err)
+
+		a := Article{
+			Author:        a.Author,
+			Body:          template.HTML(markdown),
+			Canonical:     a.Canonical,
+			ID:            a.ID,
+			LastUpdated:   lastUpdated,
+			LastUpdatedIn: t.Format("2006 Jan"),
+			LastUpdatedOn: t.Format("January 2, 2006"),
+			Published:     a.Published,
+			Redirects:     a.Redirects,
+			Tags:          a.Tags,
+			Title:         title,
+			Updated:       a.Updated,
+		}
+		articles[i] = a
+
+		for _, t := range a.Tags {
+			tags = append(tags, t)
+		}
+
+		for _, r := range a.Redirects {
+			redirectMap[r] = "/" + a.ID
+		}
+	}
+
+	sort.Strings(tags)
+	uniqTags := make([]string, 0)
+	prev := ""
+	for _, t := range tags {
+		if prev != t {
+			uniqTags = append(uniqTags, t)
+		}
+		prev = t
+	}
+
+	return data.Blog, articles, uniqTags, redirectMap
+}
+
+/*
+preProcess scans the Markdown document at filepath line-by-line,
+extracting article title and "pre-processing" article body
+which can then be passed to a Markdown compiler at the call site.
+
+When the scanner encounters an "embed" code fence like this...
+
+```embed
+example.rb id
+```
+
+...it loads the source code file at articles/code/example.rb
+and finds "magic comments" in the source like this...
+
+# begindoc: id
+puts "here"
+# enddoc: id
+
+The lines between magic comments
+are embedded back in the original code fence.
+
+Bad input in the Markdown document or source code file
+will stop the program with a non-zero exit code and error text.
+*/
+func preProcess(filepath string) (title, body string) {
+	f, err := os.Open(filepath)
+	check(err)
+	defer f.Close()
+
+	var (
+		scanner = bufio.NewScanner(f)
+		isFirst = true
+		isEmbed = false
+	)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if isFirst {
+			if strings.Index(line, "# ") == -1 {
+				exitWith("error: first line must be an h1 like: # Intro")
+			}
+
+			title = line[2:len(line)]
+			isFirst = false
+			continue
+		}
+
+		if line == "```embed" {
+			isEmbed = true
+			continue
+		}
+
+		if isEmbed {
+			parts := strings.Split(line, " ")
+			if len(parts) != 2 {
+				exitWith("error: embed line must be filename<space>id like: test.rb id")
+			}
+			filename := parts[0]
+			id := parts[1]
+
+			srcCode, err := ioutil.ReadFile(wd + "/articles/code/" + filename)
+			check(err)
+
+			sep := "begindoc: " + id + "\n"
+			begindoc := strings.Index(string(srcCode), sep)
+			if begindoc == -1 {
+				exitWith("error: embed separator not found " + sep + " in " + filename)
+			}
+			// end of comment line
+			begindoc += len(sep)
+
+			sep = "enddoc: " + id
+			enddoc := strings.Index(string(srcCode), sep)
+			if enddoc == -1 {
+				exitWith("error: embed separator not found " + sep + " in " + filename)
+			}
+			// backtrack to last newline to cut out comment character(s)
+			enddoc = strings.LastIndex(string(srcCode[0:enddoc]), "\n")
+
+			rawLines := strings.Split(string(srcCode[begindoc:enddoc]), "\n")
+			leadingWhitespace := regexp.MustCompile("(?m)(^[ \t]*)(?:[^ \t])")
+			var margin string
+			var lines []string
+			for i, l := range rawLines {
+				if i == 0 {
+					margin = leadingWhitespace.FindAllStringSubmatch(l, -1)[0][1]
+				}
+				dedented := regexp.MustCompile("(?m)^"+margin).ReplaceAllString(l, "")
+				lines = append(lines, dedented)
+			}
+
+			ext := strings.Trim(path.Ext(filename), ".")
+			body += "```" + ext + "\n" + strings.Join(lines, "\n")
+
+			isEmbed = false
+			continue
+		}
+
+		body += "\n" + line
+	}
+
+	return title, body
+}
